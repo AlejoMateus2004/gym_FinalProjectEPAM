d72fdc93bbdaf3eb5fee8b1fc6783370aa0e2885
Alejandro Mateus <102772080+AlejoMateus2004@users.noreply.github.com>
2023-12-07 10:43:59 -0500

    Initial Commit

---
+++ src/main/java/com/gymepam/GymFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gymepam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gymepam/GymFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymepam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
